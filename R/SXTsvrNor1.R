c258f7bbbce6eff6e9d59c82f495821b163588d4
omicschina <omicschina@163.com>
2017-12-27 23:42:38 +0800

    fixbugs

--- R/SXTsvrNor1.R
+++ R/SXTsvrNor1.R
@@ -1,166 +1,166 @@
-##############svr normalization function
-SXTsvrNor1 <- function(sample,
-                       QC,
-                       tags = tags,
-                       sample.order,
-                       QC.order,
-                       #used data
-                       multiple = 5,
-                       rerun = TRUE,
-                       peakplot = TRUE,
-                       path = NULL,
-                       datastyle = "tof",
-                       dimension1 = TRUE,
-                       threads = 1
-                       #parameters setting
-){
-# browser()
-  if (is.null(path)) {
-    path <- getwd()
-  }
-  path1 <- file.path(path,"svr normalization result")
-  dir.create(path1)
-
-  if (!rerun) {
-    cat("Use previous normalization data\n")
-    # Sys.sleep(1)
-    load(file.path(path1,"normalization file"))
-  }
-
-  else{
-    cat("rerun=TRUE\n")
-    # Sys.sleep(1)
-    #svr normalization is time-cosuming so save this file
-    #and load if the rerun is FALSE
-    cat("SVR normalization is finished: (peak i out of ", ncol(sample),") \n")
-    # Sys.sleep(1)
-    QC.nor <- NULL
-    sample.nor <- NULL
-
-    data <- apply(rbind(sample, QC), 2, function(x) list(x))
-    for (i in 1:ncol(QC)) {
-      all.cor <- unlist(lapply(data, function(x) {cor(data[[1]][[1]], x[[1]])}))
-      cor.peak <-
-        match(sort(all.cor, decreasing = TRUE)[2:(as.numeric(multiple)+1)], all.cor)
-
-      if (multiple != 1) {
-        svr.reg <- svm(QC[,cor.peak],QC[,i])
-      } else{
-        svr.reg <- svm(unlist(QC[,i]) ~ QC.order)
-      }
-
-      predict.QC <- summary(svr.reg)$fitted
-      QC.nor1 <- QC[,i] / predict.QC
-
-      #if the predict value is 0, then set the ratio to 0
-      QC.nor1[is.nan(unlist(QC.nor1))] <- 0
-      QC.nor1[is.infinite(unlist(QC.nor1))] <- 0
-      QC.nor1[is.na(unlist(QC.nor1))] <- 0
-      QC.nor1[which(unlist(QC.nor1) < 0)] <- 0
-
-      colnames(sample) <- colnames(QC)
-      if (multiple != 1) {
-        predict.sample <- predict(svr.reg,sample[,cor.peak])
-      }
-      else{
-        predict.sample <-
-          predict(svr.reg,data.frame(QC.order = c(sample.order)))
-      }
-
-      sample.nor1 <- sample[,i] / predict.sample
-      sample.nor1[is.nan(unlist(sample.nor1))] <- 0
-      sample.nor1[is.infinite(unlist(sample.nor1))] <- 0
-      sample.nor1[is.na(unlist(sample.nor1))] <- 0
-      sample.nor1[which(unlist(sample.nor1) < 0)] <- 0
-
-      QC.nor <- cbind(QC.nor,QC.nor1)
-      sample.nor <- cbind(sample.nor,sample.nor1)
-
-      # count <- floor(ncol(sample) * c(seq(0,1,0.01)))
-      # if (any(i == count)) {
-      #   cat(ceiling(i * 100 / ncol(sample)))
-      #   cat(" ")
-      # }
-      cat(i);cat(" ")
-
-    }
-    cat("\n")
-    cat("Normalization sample and QC are got\n")
-  }
-  ########################error
-  # dir.create("svr normalization result")
-
-  QC.median <- apply(QC,2,median)
-  if (dimension1) {
-    QC.nor <- t(t(QC.nor) * QC.median)
-    sample.nor <- t(t(sample.nor) * QC.median)
-  }
-
-  if (datastyle == "tof") {
-    colnames(QC.nor) <- colnames(sample.nor) <- tags["name",]
-  }
-  if (datastyle == "mrm") {
-    colnames(QC.nor) <- colnames(sample.nor) <- tags["name",]
-  }
-
-  save(QC.nor,sample.nor,file = file.path(path1,"normalization file"))
-
-
-  rsd <- function(x) {
-    x <- sd(x) * 100 / mean(x)
-  }
-
-
-  #following objects are the rsd of sample and QC before and after normalization
-  sample.rsd <- apply(sample,2,rsd)
-  sample.nor.rsd <- apply(sample.nor,2,rsd)
-  QC.rsd <- apply(QC,2,rsd)
-  QC.nor.rsd <- apply(QC.nor,2,rsd)
-
-
-  #sample.no.nor is the no normalization data added rsd information
-  #sample.svr is the normalization data added rsd information
-
-
-  sample.no.nor <- rbind(tags,sample.rsd,QC.rsd,sample,QC)
-  sample.svr <-
-    rbind(tags,sample.nor.rsd,QC.nor.rsd,sample.nor,QC.nor)
-
-  write.csv(t(sample.svr),file.path(path1,"data svr nor.csv"))
-
-  #generate all peaks plot
-
-  if (peakplot) {
-    path2 <- file.path(path1,"peak plot")
-    dir.create(path2)
-    if (datastyle == "tof")
-    {
-      peakplot5(
-        sample = sample,sample.nor = sample.nor,
-        QC = QC,QC.nor = QC.nor,sample.order = sample.order,
-        QC.order = QC.order,tags = tags,path = path2,
-        sample.rsd = sample.rsd,QC.rsd = QC.rsd,sample.nor.rsd =
-          sample.nor.rsd,
-        QC.nor.rsd = QC.nor.rsd
-      )
-    }
-    else {
-      peakplot6(
-        sample = sample,sample.nor = sample.nor,QC = QC,QC.nor = QC.nor,
-        sample.order = sample.order,
-        QC.order = QC.order,tags = tags,path = path2,
-        sample.rsd = sample.rsd,QC.rsd = QC.rsd,sample.nor.rsd =
-          sample.nor.rsd,
-        QC.nor.rsd = QC.nor.rsd
-      )
-    }
-  }
-
-  compare.rsd(
-    sample.rsd = sample.rsd,
-    sample.nor.rsd = sample.nor.rsd,QC.rsd = QC.rsd,QC.nor.rsd =
-      QC.nor.rsd,path = path1
-  )
-
-  cat("SVR normalization is done\n")
-}
+# ##############svr normalization function
+# SXTsvrNor1 <- function(sample,
+#                        QC,
+#                        tags = tags,
+#                        sample.order,
+#                        QC.order,
+#                        #used data
+#                        multiple = 5,
+#                        rerun = TRUE,
+#                        peakplot = TRUE,
+#                        path = NULL,
+#                        datastyle = "tof",
+#                        dimension1 = TRUE,
+#                        threads = 1
+#                        #parameters setting
+# ){
+# # browser()
+#   if (is.null(path)) {
+#     path <- getwd()
+#   }
+#   path1 <- file.path(path,"svr normalization result")
+#   dir.create(path1)
+#
+#   if (!rerun) {
+#     cat("Use previous normalization data\n")
+#     # Sys.sleep(1)
+#     load(file.path(path1,"normalization file"))
+#   }
+#
+#   else{
+#     cat("rerun=TRUE\n")
+#     # Sys.sleep(1)
+#     #svr normalization is time-cosuming so save this file
+#     #and load if the rerun is FALSE
+#     cat("SVR normalization is finished: (peak i out of ", ncol(sample),") \n")
+#     # Sys.sleep(1)
+#     QC.nor <- NULL
+#     sample.nor <- NULL
+#
+#     data <- apply(rbind(sample, QC), 2, function(x) list(x))
+#     for (i in 1:ncol(QC)) {
+#       all.cor <- unlist(lapply(data, function(x) {cor(data[[1]][[1]], x[[1]])}))
+#       cor.peak <-
+#         match(sort(all.cor, decreasing = TRUE)[2:(as.numeric(multiple)+1)], all.cor)
+#
+#       if (multiple != 1) {
+#         svr.reg <- svm(QC[,cor.peak],QC[,i])
+#       } else{
+#         svr.reg <- svm(unlist(QC[,i]) ~ QC.order)
+#       }
+#
+#       predict.QC <- summary(svr.reg)$fitted
+#       QC.nor1 <- QC[,i] / predict.QC
+#
+#       #if the predict value is 0, then set the ratio to 0
+#       QC.nor1[is.nan(unlist(QC.nor1))] <- 0
+#       QC.nor1[is.infinite(unlist(QC.nor1))] <- 0
+#       QC.nor1[is.na(unlist(QC.nor1))] <- 0
+#       QC.nor1[which(unlist(QC.nor1) < 0)] <- 0
+#
+#       colnames(sample) <- colnames(QC)
+#       if (multiple != 1) {
+#         predict.sample <- predict(svr.reg,sample[,cor.peak])
+#       }
+#       else{
+#         predict.sample <-
+#           predict(svr.reg,data.frame(QC.order = c(sample.order)))
+#       }
+#
+#       sample.nor1 <- sample[,i] / predict.sample
+#       sample.nor1[is.nan(unlist(sample.nor1))] <- 0
+#       sample.nor1[is.infinite(unlist(sample.nor1))] <- 0
+#       sample.nor1[is.na(unlist(sample.nor1))] <- 0
+#       sample.nor1[which(unlist(sample.nor1) < 0)] <- 0
+#
+#       QC.nor <- cbind(QC.nor,QC.nor1)
+#       sample.nor <- cbind(sample.nor,sample.nor1)
+#
+#       # count <- floor(ncol(sample) * c(seq(0,1,0.01)))
+#       # if (any(i == count)) {
+#       #   cat(ceiling(i * 100 / ncol(sample)))
+#       #   cat(" ")
+#       # }
+#       cat(i);cat(" ")
+#
+#     }
+#     cat("\n")
+#     cat("Normalization sample and QC are got\n")
+#   }
+#   ########################error
+#   # dir.create("svr normalization result")
+#
+#   QC.median <- apply(QC,2,median)
+#   if (dimension1) {
+#     QC.nor <- t(t(QC.nor) * QC.median)
+#     sample.nor <- t(t(sample.nor) * QC.median)
+#   }
+#
+#   if (datastyle == "tof") {
+#     colnames(QC.nor) <- colnames(sample.nor) <- tags["name",]
+#   }
+#   if (datastyle == "mrm") {
+#     colnames(QC.nor) <- colnames(sample.nor) <- tags["name",]
+#   }
+#
+#   save(QC.nor,sample.nor,file = file.path(path1,"normalization file"))
+#
+#
+#   rsd <- function(x) {
+#     x <- sd(x) * 100 / mean(x)
+#   }
+#
+#
+#   #following objects are the rsd of sample and QC before and after normalization
+#   sample.rsd <- apply(sample,2,rsd)
+#   sample.nor.rsd <- apply(sample.nor,2,rsd)
+#   QC.rsd <- apply(QC,2,rsd)
+#   QC.nor.rsd <- apply(QC.nor,2,rsd)
+#
+#
+#   #sample.no.nor is the no normalization data added rsd information
+#   #sample.svr is the normalization data added rsd information
+#
+#
+#   sample.no.nor <- rbind(tags,sample.rsd,QC.rsd,sample,QC)
+#   sample.svr <-
+#     rbind(tags,sample.nor.rsd,QC.nor.rsd,sample.nor,QC.nor)
+#
+#   write.csv(t(sample.svr),file.path(path1,"data svr nor.csv"))
+#
+#   #generate all peaks plot
+#
+#   if (peakplot) {
+#     path2 <- file.path(path1,"peak plot")
+#     dir.create(path2)
+#     if (datastyle == "tof")
+#     {
+#       peakplot5(
+#         sample = sample,sample.nor = sample.nor,
+#         QC = QC,QC.nor = QC.nor,sample.order = sample.order,
+#         QC.order = QC.order,tags = tags,path = path2,
+#         sample.rsd = sample.rsd,QC.rsd = QC.rsd,sample.nor.rsd =
+#           sample.nor.rsd,
+#         QC.nor.rsd = QC.nor.rsd
+#       )
+#     }
+#     else {
+#       peakplot6(
+#         sample = sample,sample.nor = sample.nor,QC = QC,QC.nor = QC.nor,
+#         sample.order = sample.order,
+#         QC.order = QC.order,tags = tags,path = path2,
+#         sample.rsd = sample.rsd,QC.rsd = QC.rsd,sample.nor.rsd =
+#           sample.nor.rsd,
+#         QC.nor.rsd = QC.nor.rsd
+#       )
+#     }
+#   }
+#
+#   compare.rsd(
+#     sample.rsd = sample.rsd,
+#     sample.nor.rsd = sample.nor.rsd,QC.rsd = QC.rsd,QC.nor.rsd =
+#       QC.nor.rsd,path = path1
+#   )
+#
+#   cat("SVR normalization is done\n")
+# }
---
+++ R/checkData.R
@@ -0,0 +1,319 @@
+#' @title checkData
+#' @description Check data.
+#' @author Xiaotao Shen
+#' \email{shenxt@@sioc.ac.cn}
+#' @param data The name of ms1 data.
+#' @param sample.info The name of sample information.
+#' @param path The work directory.
+#' @return Check result.
+#' @export
+#'
+
+setGeneric(name = "checkData",
+           def = function(data = "data.csv",
+                          sample.info = "sample.info.csv",
+                          path = "."){
+             data.record <- NULL
+             sample.info.record <- NULL
+
+             cat("Read data.\n")
+             cat("--------------------------------------------------------------\n")
+             data <- readr::read_csv(file.path(path, data), col_types = readr::cols(),
+                                     progress = TRUE)
+             data <- as.data.frame(data)
+             sample.info <- readr::read_csv(file.path(path, sample.info), col_types = readr::cols(),
+                                            progress = TRUE)
+             sample.info <- as.data.frame(sample.info)
+
+             ##check data, NA or space
+             if(sum(is.na(data)) > 0){
+               cat("Error: There are", sum(is.na(data)), "NAs in you data.\n")
+               data.record <- c(data.record, "Error")
+             }else{
+               # cat("OK: There are no NAs in you data.\n")
+               data.record <- c(data.record, "OK")
+             }
+
+             if(ifelse(is.na(sum(data == "") > 0), FALSE, sum(data == "") > 0)){
+               cat("Error: There are", sum(data == ""), "spaces in you data.\n")
+               data.record <- c(data.record, "Error")
+             }else{
+               # cat("OK: There are no spaces in you data.\n")
+               data.record <- c(data.record, "OK")
+             }
+
+             ######
+             data1 <- data
+             data1[is.na(data1)] <- 0
+             data1[data1 == ""] <- 0
+             data1 <- data1[,match(sample.info[,1], colnames(data1))]
+             zero.per <- apply(data1, 1, function(x){
+               sum(x == 0)/ncol(data1)
+             })
+
+             if(sum(zero.per > 0.5) > 0){
+               cat("Warning: There are peaks with zero ratio > 50% in you data.\n")
+               data.record <- c(data.record, "Warning")
+             }
+
+             #####
+
+             ##check data, component
+             data.col.name <- colnames(data)
+             if(length(data.col.name) < 7){
+               cat("Warning: There are less than 4 samples in your data, please check it.\n")
+               data.record <- c(data.record, "Warning")
+             }else{
+               cat("There are", length(data.col.name) - 3, "samples in your data.\n")
+               data.record <- c(data.record, "OK")
+             }
+
+             if(data.col.name[1] != "name"){
+               cat("Error: The first column of data is not 'name'.\n")
+               data.record <- c(data.record, "Error")
+             }else{
+               # cat("OK: The first column of data is name.\n")
+               data.record <- c(data.record, "OK")
+             }
+
+             if(data.col.name[2] != "mz"){
+               cat("Error: The second column of data is not 'mz'.\n")
+               data.record <- c(data.record, "Error")
+             }else{
+               # cat("OK: The second column of data is mz.\n")
+               data.record <- c(data.record, "OK")
+             }
+
+             if(data.col.name[3] != "rt"){
+               cat("Error: The third column of data is not 'rt'.\n")
+               data.record <- c(data.record, "Error")
+             }else{
+               # cat("OK: The third column of data is not rt.\n")
+               data.record <- c(data.record, "OK")
+             }
+
+
+             ##check data, RT
+             # if(any(data.col.name == "rt")){
+             #   rt <- as.numeric(data$rt)
+             #   if(max(rt) < 60){
+             #     cat("Warning: Please confirm that the rt is measured in seconds,\n")
+             #     data.record <- c(data.record, "Warning")
+             #   }
+             #   cat("RT range is", range(rt), ".\n")
+             # }
+
+             cat("--------------------------------------------------------------\n")
+             ##check sample.info
+             # if(ncol(sample.info) > 2){
+             #   cat("Error: The smple.info has more than two columns. Please check it.\n")
+             #   sample.info.record <- c(sample.info.record, "Error")
+             # }
+
+             if(colnames(sample.info)[1] != "sample.name"){
+               cat("Error: The first column name of sample.info must be 'sample.name'. Please check it.\n")
+               sample.info.record <- c(sample.info.record, "Error")
+             }else{
+               sample.info.record <- c(sample.info.record, "OK")
+             }
+
+             if(colnames(sample.info)[2] != "injection.order"){
+               cat("Error: The second column name of sample.info must be 'injection.order'. Please check it.\n")
+               sample.info.record <- c(sample.info.record, "Error")
+             }else{
+               sample.info.record <- c(sample.info.record, "OK")
+             }
+
+             if(colnames(sample.info)[3] != "class"){
+               cat("Error: The second column name of sample.info must be 'class'. Please check it.\n")
+               sample.info.record <- c(sample.info.record, "Error")
+             }else{
+               sample.info.record <- c(sample.info.record, "OK")
+             }
+
+
+             ##########-----
+             class <- unique(sample.info$class)
+             if(all(class %in% c("QC", "Subject"))){
+               sample.info.record <- c(sample.info.record, "OK")
+             }else{
+               cat("Error: The class of sample.info must 'QC' and 'Subject' Please check it.\n")
+               sample.info.record <- c(sample.info.record, "Error")
+             }
+
+
+
+             if(colnames(sample.info)[4] != "batch"){
+               cat("Error: The second column name of sample.info must be 'batch'. Please check it.\n")
+               sample.info.record <- c(sample.info.record, "Error")
+             }else{
+               sample.info.record <- c(sample.info.record, "OK")
+             }
+
+             if(sum(is.na(sample.info)) > 0){
+               cat("Error: There are", sum(is.na(sample.info)), "NAs in you sample.info.\n")
+               sample.info.record <- c(sample.info.record, "Error")
+             }else{
+               # cat("OK: There are no NAs in you sample.info.\n")
+               sample.info.record <- c(sample.info.record, "OK")
+             }
+
+             if(ifelse(is.na(sum(sample.info == "") > 0), FALSE, sum(sample.info == "") > 0)){
+               cat("Error: There are", sum(sample.info == ""), "spaces in you sample.info.\n")
+               sample.info.record <- c(sample.info.record, "Error")
+             }else{
+               # cat("OK: There are no spaces in you sample.info.\n")
+               sample.info.record <- c(sample.info.record, "OK")
+             }
+
+
+             # group <-  table(as.character(sample.info[,2]))
+             # group1 <- matrix(group, nrow = 1)
+             # colnames(group1) <- names(group)
+             # rownames(group1) <- "Number"
+             #
+             # cat("Group information:\n")
+             # print(group1)
+             #
+             # if(any(group1[1,] < 2)){
+             #   cat("Error: ",group1[1,which(group1[,1] < 3)],
+             #       ifelse(length(which(group1[,1]) < 3) > 1, "have", "has"),
+             #       "less than 3 samples, please check it.\n"
+             #   )
+             #   sample.info.record <- c(sample.info.record, "Error")
+             # }else{
+             #   sample.info.record <- c(sample.info.record, "OK")
+             # }
+
+
+             data.col.name <- setdiff(data.col.name, c("name", "mz", "rt"))
+             sample.name <- as.character(sample.info[,1])
+
+             if(any(sort(data.col.name) != sort(sample.name))){
+               cat("Error: The sample names in data and sample.info are not same.\n")
+               sample.info.record <- c(sample.info.record, "Error")
+             }else{
+               sample.info.record <- c(sample.info.record, "OK")
+             }
+
+
+             ###if the name of sample is begin with number, add "Sample" before it
+             data.col.name <- setdiff(data.col.name, c("name", "mz", "rt"))
+             sample.name <- as.character(sample.info[,1])
+
+             if(any(substr(x = sample.name, start = 1, stop = 1) %in% 0:9)){
+               cat("Warning: The names of samples should not start with a number.\n")
+               sample.info.record <- c(sample.info.record, "Warning")
+
+               idx1 <- which(substr(x = sample.name, start = 1, stop = 1) %in% 0:9)
+               idx2 <- which(substr(x = data.col.name, start = 1, stop = 1) %in% 0:9)
+
+               sample.name[idx1] <- paste("Sample", sample.name[idx1], sep = "")
+               data.col.name[idx2] <- paste("Sample", data.col.name[idx2], sep = "")
+
+               sample.info[,1] <- sample.name
+               colnames(data)[-which(colnames(data) %in% c("name", "mz", "rt"))] <- data.col.name
+               readr::write_csv(data, file.path(path, "data.csv"))
+               readr::write_csv(sample.info, file.path(path, "sample.info.csv"))
+
+             }else{
+               sample.info.record <- c(sample.info.record, "OK")
+             }
+
+             # cat("--------------------------------------------------------------\n")
+             # ##ms2.file
+             # switch(ms2.type,
+             #        "mgf" = {ms2.file <- grep("mgf", dir(path), value = TRUE)},
+             #        "msp" = {ms2.file <- grep("msp", dir(path), value = TRUE)},
+             #        "mzXML" = {ms2.file <- grep("mzXML", dir(path), value = TRUE)})
+             #
+             # if(length(ms2.file) == 0){
+             #   cat("Error: There are no ms2 data.\n")
+             #   ms2.file.record <- c(ms2.file.record, "Error")
+             # }else{
+             #   cat("There are", length(ms2.file), "ms2 data.\n")
+             #   ms2.file.record <- c(ms2.file.record, "OK")
+             #   cat("Total size of ms2 data is", sum(file.size(ms2.file)/(1024*1024)), "M.\n")
+             # }
+
+             cat("--------------------------------------------------------------\n")
+             cat("Summary:\n")
+
+             stat <- lapply(list(data.record, sample.info.record),
+                            function(x) {
+                              return(c(ifelse(all(x!="Error"), "Valid", "Invalid"),
+                                       sum(x=="OK"), sum(x=="Warning"), sum(x=="Error")))
+                            })
+             stat <- do.call(rbind, stat)
+             colnames(stat) <- c("Check result","OK", "Warning", "Error")
+             rownames(stat) <- c("data", "sample.info")
+
+             print(stat, quote = FALSE)
+             cat("\n")
+
+             cat("\n")
+             cat("data:\n")
+             if(all(data.record != "Error")){
+               cat("data is valid.\n")
+             }else{
+               if(sum(data.record == "Warning") > 0){
+                 cat("There", ifelse(sum(data.record == "Warning") > 1, "are", "is"),
+                     sum(data.record == "Warning"),
+                     ifelse(sum(data.record == "Warning") > 1, "Warnings", "Warning"),
+                     "in your data. Please check it according to the information.\n")
+               }
+
+               if(sum(data.record == "Error") > 0){
+                 cat("There", ifelse(sum(data.record == "Error") > 1, "are", "is"),
+                     sum(data.record == "Error"),
+                     ifelse(sum(data.record == "Error") > 1, "Errors", "Error"),
+                     "in your data. Please check it according to the information.\n")
+               }
+
+             }
+
+
+
+             cat("\n")
+             cat("sample.info:\n")
+             if(all(sample.info.record != "Error")){
+               cat("sample.info is valid.\n")
+             }else{
+               if(sum(sample.info.record == "Warning") > 0){
+                 cat("There", ifelse(sum(sample.info.record == "Warning") > 1, "are", "is"),
+                     sum(sample.info.record == "Warning"),
+                     ifelse(sum(sample.info.record == "Warning") > 1, "Warnings", "Warning"),
+                     "in your sample.info. Please check it according to the information.\n")
+               }
+
+               if(sum(sample.info.record == "Error") > 0){
+                 cat("There", ifelse(sum(sample.info.record == "Error") > 1, "are", "is"),
+                     sum(sample.info.record == "Error"),
+                     ifelse(sum(sample.info.record == "Error") > 1, "Errors", "Error"),
+                     "in your sample.info. Please check it according to the information.\n")
+               }
+
+             }
+
+             # cat("\n")
+             # cat("ms2.file:\n")
+             # if(all(ms2.file.record != "Error")){
+             #   cat("ms2.file are valid.\n")
+             # }else{
+             #   if(sum(ms2.file.record == "Warning") > 0){
+             #     cat("There", ifelse(sum(ms2.file.record == "Warning") > 1, "are", "is"),
+             #         sum(ms2.file.record == "Warning"),
+             #         ifelse(sum(ms2.file.record == "Warning") > 1, "Warnings", "Warning"),
+             #         "in your ms2.file. Please check it according to the information.\n")
+             #   }
+             #
+             #   if(sum(ms2.file.record == "Error") > 0){
+             #     cat("There", ifelse(sum(ms2.file.record == "Error") > 1, "are", "is"),
+             #         sum(ms2.file.record == "Error"),
+             #         ifelse(sum(ms2.file.record == "Error") > 1, "Errors", "Error"),
+             #         "in your ms2.file. Please check it according to the information.\n")
+             #   }
+             #
+             # }
+             stat <- stat
+           })
